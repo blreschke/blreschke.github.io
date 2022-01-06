09f073f17c7509209eb4c2298265149463723ea6
blreschke <reschke3@illinois.edu>
2022-01-06 15:00:33 -0600

    react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Board(props) {
+  return 
+}
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Wrapper">
+        <div className="Buttons">
+          <button className='New4'>New 4x4</button>
+          <button className='New5'>New 5x5</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
